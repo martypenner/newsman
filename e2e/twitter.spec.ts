d590bece9df2c78947c6ce026d9c39d7117767e6
Marty Penner <martypenner@users.noreply.github.com>
2023-11-26 23:49:20 -0500

    feat: add screenshots

--- e2e/twitter.spec.ts
+++ e2e/twitter.spec.ts
@@ -1,10 +1,7 @@
 import { test, type Page } from '@playwright/test';
+import sharp from 'sharp';
 
 const authFile = '.auth/twitter.json';
-
-test.beforeEach(async ({ page }) => {
-	await page.goto('https://twitter.com');
-});
 
 test.describe('Twitter', () => {
 	test('should log in', async ({ page }) => {
@@ -22,6 +19,13 @@ test.describe('Twitter', () => {
 
 		let tweetContents = [];
 
+		await page.locator('[role=progressbar] svg').waitFor({ state: 'hidden' }); // Wait a bit for new tweets to load
+		// TODO: use [aria-labelledby="accessible-list-1"] for just tweets, no header, footer, sidebar, etc.
+		// Doing it now causes a bunch of black bars to show up in the screenshot. Virtualization maybe?
+		await page.screenshot({
+			path: `screenshots/twitter-0.png`
+		});
+
 		// Scroll through the timeline
 		let previousHeight = 0;
 		for (let i = 0; i < NUM_TIMES_TO_SCROLL; i++) {
@@ -35,7 +39,11 @@ test.describe('Twitter', () => {
 			}
 
 			previousHeight = currentHeight;
-			await page.waitForTimeout(2_000); // Wait a bit for new tweets to load
+			await page.locator('[role=progressbar] svg').waitFor({ state: 'hidden' }); // Wait a bit for new tweets to load
+
+			await page.screenshot({
+				path: `screenshots/twitter-${i + 1}.png`
+			});
 
 			const tweets = page.getByTestId('tweet');
 			// TODO: rank and prioritize these somehow
@@ -52,14 +60,17 @@ test.describe('Twitter', () => {
 		}
 		tweetContents = new Set(tweetContents);
 
-		await page.screenshot({ fullPage: true });
+		stitchImages(
+			Array.from({ length: NUM_TIMES_TO_SCROLL + 1 }, (_, i) => `screenshots/twitter-${i}.png`),
+			'screenshots/twitter-full.png'
+		);
 
 		// await page.pause();
 
 		console.log(tweetContents.size);
 		console.log(Array.from(tweetContents));
 
-		// TODO: show more on tweets
+		// TODO: show more on tweets using [data-testid="tweet"] span "show more"
 		// TODO: filter out ads
 		// TODO: filter out unneeded parts of tweets, like metadata
 		// TODO: go to specific users' timelines and grab their latest stuff. Will require a db.
@@ -74,4 +85,42 @@ async function login(page: Page) {
 	await page.getByRole('button', { name: 'Next' }).click();
 	await page.pause();
 	await page.getByTestId('LoginForm_Login_Button').click();
+}
+
+async function stitchImages(imagePaths: string[], outputPath: string): Promise<void> {
+	const images = await Promise.all(
+		imagePaths.map((path) =>
+			sharp(path)
+				.metadata()
+				.then((metadata) => ({ path, metadata }))
+		)
+	);
+
+	const totalHeight = images.reduce((sum, { metadata }) => sum + (metadata?.height ?? 0), 0);
+	const maxWidth = Math.max(...images.map(({ metadata }) => metadata.width ?? 0));
+
+	const output = sharp({
+		create: {
+			width: maxWidth,
+			height: totalHeight,
+			channels: 4,
+			background: { r: 0, g: 0, b: 0, alpha: 0 }
+		}
+	});
+
+	let y = 0;
+	output.composite(
+		images.map(({ path, metadata }, i) => {
+			const image = {
+				input: path,
+				top: y,
+				left: 0
+			};
+			y += metadata.height ?? 0;
+
+			return image;
+		})
+	);
+
+	await output.toFile(outputPath);
 }
