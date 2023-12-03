5d24fffdf8d7e36effd11b5d33021fc5025bf4d2
Marty Penner <martypenner@users.noreply.github.com>
2023-12-03 12:17:05 -0500

    feat: bring back other steps in workflow

--- src/lib/inngest.ts
+++ src/lib/inngest.ts
@@ -19,20 +19,19 @@ export const functions = [
 		async ({ step, logger }) => {
 			const screenshot = await step.run('get-screenshots', async () => {
 				const screenshot = await getScreenshots();
-				logger.info('Screenshot:', screenshot);
 				return screenshot;
 			});
-			// const content = await step.run('extract-tweets', async () => {
-			// logger.info('Extracting tweets...');
-			// return await extractTweets(screenshot);
-			// });
-			//
-			// if (content != null && !content.startsWith(`I'm sorry`)) {
-			// 	await step.run('send-email-summary', async () => {
-			// 	logger.info('Sending email summary...')
-			//await sendEmailSummary(content)
-			// 	});
-			// }
+			const content = await step.run('extract-tweets', async () => {
+				logger.info('Extracting tweets...');
+				return await extractTweets(screenshot);
+			});
+
+			if (content != null && !content.startsWith(`I'm sorry`)) {
+				await step.run('send-email-summary', async () => {
+					logger.info('Sending email summary...');
+					await sendEmailSummary(content);
+				});
+			}
 
 			logger.info('Done!');
 		}
