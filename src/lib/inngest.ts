d48651a9d8c81b0301450e737e40f45a4f91130e
Marty Penner <martypenner@users.noreply.github.com>
2023-12-13 05:43:34 -0500

    feat: allow triggering inngest function using event

--- src/lib/inngest.ts
+++ src/lib/inngest.ts
@@ -4,7 +4,7 @@ import { getScreenshots } from './get-screenshots';
 import { sendEmailSummary } from './send-email-summary';
 
 type Events = {
-	// no-op
+	'app/browser.triggered': object;
 };
 
 export const inngest = new Inngest({
@@ -14,28 +14,31 @@ export const inngest = new Inngest({
 
 export const functions = [
 	inngest.createFunction(
-		{ id: 'get-news' },
+		{ id: 'get-news-scheduled' },
 		{ cron: 'TZ=America/Toronto 0 9,21 * * *' },
-		async ({ step, logger }) => {
-			const screenshot = await step.run('get-screenshots', async () => {
-				const screenshot = await getScreenshots();
-				return screenshot;
-			});
-			logger.info(screenshot);
-			const content = await step.run('extract-tweets', async () => {
-				logger.info('Extracting tweets...');
-				return await extractTweets(Buffer.from(screenshot.data));
-			});
-			logger.info(content);
-
-			if (content.data != null && !content.data.startsWith(`I'm sorry`)) {
-				await step.run('send-email-summary', async () => {
-					logger.info('Sending email summary...');
-					await sendEmailSummary(content.data);
-				});
-			}
-
-			logger.info('Done!');
-		}
-	)
+		getNews
+	),
+	inngest.createFunction({ id: 'get-news-from-event' }, { event: 'app/browser.triggered' }, getNews)
 ];
+
+async function getNews({ step, logger }) {
+	const screenshot = await step.run('get-screenshots', async () => {
+		const screenshot = await getScreenshots();
+		return screenshot;
+	});
+	logger.info(screenshot);
+	const content = await step.run('extract-tweets', async () => {
+		logger.info('Extracting tweets...');
+		return await extractTweets(Buffer.from(screenshot.data));
+	});
+	logger.info(content);
+
+	if (content.data != null && !content.data.startsWith(`I'm sorry`)) {
+		await step.run('send-email-summary', async () => {
+			logger.info('Sending email summary...');
+			await sendEmailSummary(content.data);
+		});
+	}
+
+	logger.info('Done!');
+}
